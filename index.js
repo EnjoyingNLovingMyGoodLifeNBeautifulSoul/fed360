ca2e8f5b15ee854842d4c833019f98f2baa68a49
David Lee <david.lee@census.gov>
2016-04-18 20:56:17 -0400

    added check for competency endorsement

--- index.js
+++ index.js
@@ -976,7 +976,7 @@ app.post('/saveEndorsements', function(request, response) {
           }
         }
 
-        console.log('endorsement array created ');
+        console.log('endorsement array created : ' + Object.keys(endorsements).length;
         //console.log(endorsements);
 
         async.each(endorsements, function(endorsement, callback2) {
