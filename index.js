d96af997ff250b6768b7b7777576c86b9be3261c
David Lee <david.lee@census.gov>
2016-06-25 23:00:20 -0400

    temporarily commented out JSON due to error

--- index.js
+++ index.js
@@ -2102,11 +2102,11 @@ app.post('/updateCompetencies', function(request, response) {
 
           // blank values [''] are not allowed.  Either user [] or have a value like ['a']
           var competencyJSON = {
-            'Name': newCompetency.name,
-            'Short Description': newCompetecy.description,
-            'Link': newCompetency.readMoreURL,
-            'Type': 'Team',
-            'people': [profileJSON.id]
+          //  'Name': newCompetency.name,
+          //  'Short Description': newCompetecy.description,
+          //  'Link': newCompetency.readMoreURL,
+          //  'Type': 'Team',
+          //  'people': [profileJSON.id]
           };
 
           console.log(competencyJSON);
