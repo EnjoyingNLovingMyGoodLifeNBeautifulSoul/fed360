e62129101075ea7d0ca7be7e0433a1ad789ce0ee
David Lee <david.lee@census.gov>
2016-08-03 16:03:48 -0400

    switched to single organization for now

--- index.js
+++ index.js
@@ -1538,7 +1538,7 @@ function saveProfile(request, response) {
         console.log('processing organization');
 		console.log('organizations: ' + profileJSON.organization);
 		//var organizations = profileJSON.organization.split(',');
-		var organizations = [profileJSON.organization];
+		var organizations = [profileJSON.organization[0]]];
 		async.each(organizations, function(organization, callback2) {
 			console.log('async organizationRecord size: ' + organizationRecords.length);
 			getOrganization(organization, profileJSON, callback2, organizationRecords);
