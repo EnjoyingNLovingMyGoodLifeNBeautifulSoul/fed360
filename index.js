aeffef5e1fead0cec56f3e3092fa95b50c309023
David Lee <david.lee@census.gov>
2016-08-03 15:33:48 -0400

    set single organization to array

--- index.js
+++ index.js
@@ -1538,7 +1538,7 @@ function saveProfile(request, response) {
         console.log('processing organization');
 		console.log('organizations: ' + profileJSON.organization);
 		//var organizations = profileJSON.organization.split(',');
-		var organizations = profileJSON.organization;
+		var organizations = [profileJSON.organization];
 		async.each(organizations, function(organization, callback2) {
 			console.log('async organizationRecord size: ' + organizationRecords.length);
 			getOrganization(organization, profileJSON, callback2, organizationRecords);
