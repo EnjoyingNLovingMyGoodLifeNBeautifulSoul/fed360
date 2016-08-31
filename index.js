cdb8f380a81fd3a86295a99d21602b4d08c90a3c
David Lee <david.lee@census.gov>
2016-08-31 16:19:15 -0400

    updated position id to position.id

--- index.js
+++ index.js
@@ -2051,7 +2051,7 @@ function updatePositions(profileJSON, profileRecord, organizationRecords, allPos
 				newOrganizations.push(key);
 			}
 
-			base('Position').update(positionRecord.getId(), {
+			base('Position').update(position.id, {
 				"Official Title": position.officialtitle,
 				"People": people,
 				"Organizations": newOrganizations
